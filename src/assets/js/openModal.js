3dfa74fb44ffbe2ff58c635ceb8f52d53b7b7dd1
Stéphanie Vanoverberghe <orangestreet@live.fr>
2024-05-02 16:33:44 +0200

    fix: menu trigger mobile

--- src/assets/js/openModal.js
+++ src/assets/js/openModal.js
@@ -1,22 +1,84 @@
 const btnModal = document.querySelector('#openModal');
 const modalClose = document.querySelector('#modalClose');
 const modal = document.querySelector('#modalRegister');
+const menu = document.querySelector('#menu');
+const menuTrigger = document.querySelector('#menuTrigger');
+const menuTriggerIcons = menuTrigger.querySelectorAll('.menu__icon');
 
-// FUNCTIONS
+/**
+ * Opening the modal
+ */
 const openModal = () => {
     modal.style.display = "block";
 }
+btnModal.addEventListener('click', openModal);
 
+/**
+ * Closing the modal
+ */
 const closeModal = () => {
     modal.style.display = "none";
 }
-
-// ADDEVENTLISTENER
-btnModal.addEventListener('click', openModal);
 modalClose.addEventListener('click', closeModal);
 
+/**
+ * Clicking outside the modal, close it 
+ */
 window.addEventListener('click', event => {
     if (event.target == modal) {
-        modal.style.display = "none";
+        closeModal();
     }
 })
+
+/**
+ * Switch between the opening and closing menu 
+ */
+const toggleMenu = () => {
+    if (menu.style.display === 'block') {
+        closeMenu();
+    } else {
+        openMenu();
+    }
+};
+
+/**
+ * Opening the menu by clicking on the burger menu
+ */
+const openMenu = () => {
+    menu.style.display = 'block';
+    menuTriggerIcons[0].classList.add('icon__hidden');
+    menuTriggerIcons[1].classList.remove('icon__hidden');
+};
+
+/**
+ * Closing the menu by clicking on the burger menu
+ */
+const closeMenu = () => {
+    menu.style.display = 'none';
+    menuTriggerIcons[0].classList.remove('icon__hidden');
+    menuTriggerIcons[1].classList.add('icon__hidden');
+};
+
+menuTrigger.addEventListener('click', toggleMenu);
+
+/**
+ * Checks if the clicked item is neither the menu nor a descendant of menu
+ * Closing the menu if you clicked outside
+ */
+window.addEventListener('click', event => {
+    if (!menu.contains(event.target) && !menuTrigger.contains(event.target)) {
+        closeMenu();
+    }
+});
+
+/**
+ *  Disappearance of the menu when switching to an LG screen size
+ */
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 1050) {
+        menu.style.display = 'none';
+        menuTriggerIcons[0].classList.remove('icon__hidden');
+        menuTriggerIcons[1].classList.add('icon__hidden');
+    }
+});
+
