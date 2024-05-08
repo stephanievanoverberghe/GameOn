1d3d3dec4125fd2765804025f9173a5db1bdf8dc
Stéphanie Vanoverberghe <orangestreet@live.fr>
2024-05-08 17:45:40 +0200

    fix: update validate form

--- src/assets/js/formModal.js
+++ src/assets/js/formModal.js
@@ -1,69 +1,4 @@
-/**
- * Script dedicated to form management
- */
-
-// Selecting form and error messages
-const form = document.querySelector('form');
-const errorMessage = document.querySelectorAll('.form__error');
-
-// Selecting form inputs
-const firstname = document.querySelector('#firstname');
-const lastname = document.querySelector('#lastname');
-const email = document.querySelector('#email');
-const birthday = document.querySelector('#birthday');
-const concoursParticipate = document.querySelector('#numberParticipate');
-const boutonsRadios = document.querySelectorAll('input[name="location"]');
-const conditionForm = document.getElementById('checkbox1');
-
-// Succès send form
-const formSucces = document.querySelector('.succes__submit');
-const modalBody = document.querySelector('.modal__body');
-
-// REGEX for validation
-const regexName = /^[A-Za-zÀ-ÖØ-öø-ÿ]{2,}$/;
-const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-const regexDate = /^(19|20)\d\d-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/;
-const regexConcours = /^(?:[0-9]|[1-9][0-9])$/;
-
-/**
- * Function to toggle error messages based on validation
- * * @param {object} params
- * * @param {number} params.index
- * * @param {boolean} params.validate
- * 
- */
-const validateForm = ({ index, validate }) => {
-    if (validate) {
-        errorMessage[index].style.display = "none";
-    } else {
-        errorMessage[index].style.display = "block";
-    }
-};
-
-/**
- * Functions to validate each form field
- */
-const validateFirstname = () => {
-    validateForm({ index: 0, validate: regexName.test(firstname.value) });
-};
-
-const validateLastname = () => {
-    validateForm({ index: 1, validate: regexName.test(lastname.value) });
-};
-
-const validateMail = () => {
-    validateForm({ index: 2, validate: regexEmail.test(email.value) });
-};
-
-const validateBirthday = () => {
-    const userBirthday = new Date(birthday.value);
-    const minAgeDate = getDate18YearsAgo();
-    if (regexDate.test(birthday.value) && userBirthday <= minAgeDate) {
-        validateForm({ index: 3, validate: true });
-    } else {
-        validateForm({ index: 3, validate: false });
-    }
-};
+const form = document.querySelector('.form');
 
 const getDate18YearsAgo = () => {
     const today = new Date();
@@ -71,67 +6,93 @@ const getDate18YearsAgo = () => {
     return today;
 };
 
-const validateConcoursParticipate = () => {
-    if (regexConcours.test(concoursParticipate.value)) {
-        validateForm({ index: 4, validate: true });
-    } else {
-        validateForm({ index: 4, validate: false });
+const validationConfig = {
+    firstname: {
+        rule: value => /^[A-Za-zÀ-ÖØ-öø-ÿ]{2,}$/.test(value),
+        errorMessage: 'Le prénom doit contenir au moins 2 caractères.'
+    },
+    lastname: {
+        rule: value => /^[A-Za-zÀ-ÖØ-öø-ÿ]{2,}$/.test(value),
+        errorMessage: 'Le nom doit contenir au moins 2 caractères.'
+    },
+    email: {
+        rule: value => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value),
+        errorMessage: 'Email non valide.'
+    },
+    birthday: {
+        rule: value => {
+            const regexDate = /^(19|20)\d\d-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/;
+            const userBirthday = new Date(value);
+            const minAgeDate = getDate18YearsAgo();
+            return regexDate.test(value) && userBirthday <= minAgeDate;
+        },
+        errorMessage: 'Vous devez être âgé de plus de 18 ans.'
+    },
+    participation: {
+        rule: value => parseInt(value, 10) >= 0,
+        errorMessage: 'Le nombre de participations n\'est pas valide.'
+    },
+    location: {
+        rule: value => value !== '',
+        errorMessage: 'Veuillez sélectionner un tournoi.'
+    },
+    conditions: {
+        rule: value => value === true,
+        errorMessage: 'Vous devez accepter les conditions d’utilisation.'
     }
 };
 
-const validateLocation = () => {
-    let checked = false;
-    boutonsRadios.forEach(radio => {
-        if (radio.checked) {
-            checked = true;
-        }
-    });
-    validateForm({ index: 5, validate: checked });
-};
-
-const validateCondition = () => {
-    validateForm({ index: 6, validate: conditionForm.checked });
-};
-
 /**
- * Function to check if the entire form is valid
- * @returns {boolean}
+ * Sert à vérifier si les données saisies dans le formulaire respectent les règles spécifiées pour chaque champ.
+ * 
+ * @param {object} formData // Contient toutes les données saisies dans le formulaire
+ * @param {object} config // Contient la configuration de validation pour chaque champ du formulaire : rules et errorMessage
+ * @returns 
  */
-const isFormValid = () => {
+const validateForm = (formData, config) => {
+    // Permet de voir si toutes les données sont valides
     let isValid = true;
-    errorMessage.forEach(error => {
-        if (error.style.display !== "none") {
+    /**
+     * Object.entries() créer un tableau des entrée de l'objet config où chaque entrée est un tableau [clé, valeur].
+     * fieldname représente la clé, le nom du champ
+     * {rule, errorMessage} déstructure l'objet de configuration pour ce champ.
+     */
+    Object.entries(config).forEach(([fieldName, { rule, errorMessage }]) => {
+        // Sélectionne l'élément du formulaire basé sur son attribut "name" qui correspond au "fieldname"
+        const inputElement = form.querySelector(`[name="${fieldName}"]`);
+        // Récupère la valeur du champ. 
+        // Si le type de l'élément est une case à cocher, il récupère la propriété "checked"
+        // Sinon il récupère la valeur du champ à partir de l'objet "formData"
+        const value = inputElement.type === 'checkbox' ? inputElement.checked : formData.get(fieldName);
+        const errorElement = inputElement.closest('.form__data').querySelector('.data__error');
+
+        console.log(`Validating ${fieldName}:`, value);
+
+        // Teste si la valeur du champ respecte la règle de validation.
+        if (!rule(value)) {
+            errorElement.textContent = errorMessage;
             isValid = false;
+
+            console.log(`${fieldName} error: ${errorMessage}`);
+
+        } else {
+            errorElement.textContent = '';
         }
     });
     return isValid;
 };
 
-// Event listeners for input validation
-firstname.addEventListener('input', validateFirstname);
-lastname.addEventListener('input', validateLastname);
-email.addEventListener('input', validateMail);
-birthday.addEventListener('input', validateBirthday);
-concoursParticipate.addEventListener('input', validateConcoursParticipate);
-boutonsRadios.forEach(radio => {
-    radio.addEventListener('change', validateLocation);
-});
-conditionForm.addEventListener("change", validateCondition);
+const handleSubmit = e => {
+    e.preventDefault();
 
-/**
- * Event handler to prevent form submission if invalid
- * @param {SubmitEvent} e 
- */
-const handleForm = e => {
-    if (isFormValid()) {
-        // Si le formulaire est valide, affichez le modal de succès
-        modalSucces.style.display = "block";
-        // Masquez le formulaire en lui retirant la classe modal__body
-        modalBody.classList.remove("modal__body");
-        // Ajoutez une classe pour masquer le formulaire (si nécessaire)
-        modalBody.classList.add("hidden");
-        // Empêchez la soumission du formulaire
-        e.preventDefault();
+    const formData = new FormData(form);
+    const isValid = validateForm(formData, validationConfig);
+
+    if (isValid) {
+        console.log('Toutes les données sont valides !');
+    } else {
+        console.log('Validation échouée, le formulaire n\'est pas envoyé !');
     }
-};
-form.addEventListener('submit', handleForm);
+}
+
+form.addEventListener('submit', handleSubmit);
